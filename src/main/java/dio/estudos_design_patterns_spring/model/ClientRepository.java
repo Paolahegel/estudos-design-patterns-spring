f4969d8cbfaf8688b1fc33dd877e6cf2c2c91dd8
Paola Moura <169110243+Paolahegel@users.noreply.github.com>
2025-03-30 21:45:33 -0300

    Delete src directory

--- src/main/java/dio/estudos_design_patterns_spring/model/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package dio.estudos_design_patterns_spring.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<Client, Long> {
-}
